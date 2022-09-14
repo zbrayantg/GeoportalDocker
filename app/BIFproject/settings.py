a83105745a156c5a806b2e6326c7992a0a8f6d1a
Brayant Gualdron <zbgualdron@gmail.com>
2022-09-14 17:12:27 -0500

    Add a staging for pre production

--- app/BIFproject/settings.py
+++ app/BIFproject/settings.py
@@ -144,3 +144,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
